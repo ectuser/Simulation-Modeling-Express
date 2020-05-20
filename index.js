0dfa600d1d50b7b7ca8e607763ad2e694dcfa7e0
ectuser <m.ectuser@gmail.com>
2020-05-21 00:53:16 +0700

    Added some react project to check how to deploy it

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, 'public/build')));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

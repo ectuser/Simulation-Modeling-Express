bc8c764b9932b51cbcff7378360577e9903c9478
ectuser <m.ectuser@gmail.com>
2020-05-22 21:53:48 +0700

    First part of task is done

--- server/businessLogic/lab14.js
+++ server/businessLogic/lab14.js
@@ -1,5 +1,6 @@
 const lab14 = async (average, variance, experimentsAmount) => {
     const intervalsAmount = Math.ceil(Math.log(experimentsAmount)) + 1;
+    // const intervalsAmount = Math.ceil(Math.sqrt(experimentsAmount)) + 1;
 
     const values = doExperiments(average, variance, experimentsAmount);
 
@@ -15,19 +16,33 @@ const lab14 = async (average, variance, experimentsAmount) => {
     console.log("Selections: ", selections);
     const probabilities = getProbabilities(selections, experimentsAmount);
     console.log("Probabilities: ", probabilities);
+
+
+    const allArea = getAllArea(intervalValue, probabilities);
+    const coef = 1 / allArea;
+    const density = getAllDensity(probabilities, coef);
+    console.log(coef);
+    // Эта штука нужна, чтобы сумма площадей фигур была = 1
+
+
+    console.log("All area: ", allArea);
     const theoreticalProbabilities = getTheoreticalProbabilities(intervals, variance, average);
     console.log("Theoretical probabilities", theoreticalProbabilities);
 
     const resultObject = {
         labels: intervals,
         probabilities: {
-            practical: probabilities,
+            practical: density,
             theoretical: theoreticalProbabilities
         }
     };
     return resultObject;
 
 }
+
+const getAllDensity = (probabilities, coef) => probabilities.map((item, i) => item * coef);
+
+const getAllArea = (intervalValue, probabilities) => [...probabilities.map((item, i) => item * intervalValue)].reduce((a,b) => a + b);
 
 const getTheoreticalProbabilities = (intervals, variance, average) => intervals.map((x, i) => ((Math.E ** (-((x - average) ** 2) / (2 * variance))) / (Math.sqrt(variance) * Math.sqrt(2 * Math.PI))));
 

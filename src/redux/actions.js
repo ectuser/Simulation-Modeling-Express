b565669292c0ef468c9ac2ac11dea655e6a259a9
ectuser <m.ectuser@gmail.com>
2020-05-23 19:58:16 +0700

    Added saga support for 14 lab

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 export const setIsLoading = (status) => ({ type: "SET_IS_LOADING", payload: status });
 export const setIsLoading14 = (status) => ({ type: "SET_IS_LOADING_14", payload: status });
 export const setData = (data) => ({type : "SET_DATA", payload: data})
@@ -8,17 +7,21 @@ export const solveData13 = (lambda, range, experimentsAmount) => ({
     type: "SOLVE_DATA_13",
     payload: {lambda, range, experimentsAmount}
 })
+export const solveData14 = (data) => ({
+    type: "SOLVE_DATA_14",
+    payload: data
+})
 
-export const solveData14 = (data) => async (dispatch) => {
-    dispatch(setIsLoading14(true));
-    try {
-        const outputServerData = await axios.get('/lab-14', {params : data});
-        const outputData = outputServerData.data;
-        console.log(outputData);
-        dispatch(setData14(outputData));
-    } catch (error) {
-        console.error(error);
-    } finally{
-        dispatch(setIsLoading14(false));
-    }
-}
+// export const solveData14 = (data) => async (dispatch) => {
+//     dispatch(setIsLoading14(true));
+//     try {
+//         const outputServerData = await axios.get('/lab-14', {params : data});
+//         const outputData = outputServerData.data;
+//         console.log(outputData);
+//         dispatch(setData14(outputData));
+//     } catch (error) {
+//         console.error(error);
+//     } finally{
+//         dispatch(setIsLoading14(false));
+//     }
+// }
--- src/redux/sagas/saga.js
+++ src/redux/sagas/saga.js
@@ -1,5 +1,5 @@
 import { put, takeEvery, all } from "redux-saga/effects";
-import { setIsLoading, setData, solveData13 } from "../actions";
+import { setIsLoading, setData, setIsLoading14, setData14 } from "../actions";
 import axios from "axios";
 
 function* solveData13Worker(action) {
@@ -19,12 +19,22 @@ function* solveData13Worker(action) {
 }
 
 function* solveData14Worker(action){
-    
+    yield put(setIsLoading14(true));
+    try {
+        const outputServerData = yield axios.get('/lab-14', {params : action.payload});
+        const outputData = outputServerData.data;
+        console.log(outputData);
+        yield put(setData14(outputData));
+    } catch (error) {
+        console.error(error);
+    } finally{
+        yield put(setIsLoading14(false));
+    }
 }
 
 
 
 
 export function* mainSaga() {
-    yield all([takeEvery("SOLVE_DATA_13", solveData13Worker)]);
+    yield all([takeEvery("SOLVE_DATA_13", solveData13Worker), takeEvery("SOLVE_DATA_14", solveData14Worker)]);
 }

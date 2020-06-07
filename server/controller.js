65ef7c19b3cbe036cb7a0be1e0e57ff80ea423fe
ectuser <m.ectuser@gmail.com>
2020-06-07 22:22:30 +0700

    Added business logic

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,5 @@
-const lab13 = require('./businessLogic/lab13');
-const lab14 = require('./businessLogic/lab14');
+const lab13 = require('./services/lab13');
+const lab14 = require('./services/lab14');
 
 const Lab13Controller = async (req, res) => {
     const lambda = req.query.lambda;
@@ -7,14 +7,19 @@ const Lab13Controller = async (req, res) => {
     const experimentsAmount = req.query.experimentsAmount;
     let data = await lab13(Number(lambda), Number(range), Number(experimentsAmount)); // output should be object
     res.json(data);
-}
+};
 
 const Lab14Controller = async (req, res) => {
     let output = await lab14(Number(req.query.average), Number(req.query.variance), Number(req.query.experimentsAmount)); // output should be object
     res.json(output);
-}
+};
+
+const Lab15Controller = (req, res) => {
+    
+};
 
 module.exports = {
     Lab13Controller : Lab13Controller,
-    Lab14Controller : Lab14Controller
+    Lab14Controller : Lab14Controller,
+    Lab15Controller : Lab15Controller
 };
---
+++ server/services/lab15.js
@@ -0,0 +1,40 @@
+const lab15 = ({events, t, i}) => {
+    // states : 
+    // 0 - clear
+    // 1 - cloudy
+    // 2 - overcast
+    const states = ["clear", "cloudy", "overcast"];
+    
+    const coefs = [
+        [-0.4, 0.3, 0.1],
+        [0.4, -0.8, 0.4],
+        [0.1, 0.4, -0.5]
+    ];
+
+    const alpha = 0.05;
+
+    for (let index = 0; index < 10; index++){
+        let tao = Math.log(alpha) / coefs[i][i];
+        t += tao;
+
+        const probabilities = getProbabilities([...coefs], i);
+        const distribution = getDistribution([...probabilities]);
+        const probability = Math.random();
+        i = getEvent(distribution, probability);
+
+        events.push(states[i], t);
+    }
+    return {events, t, i};
+}
+
+const getProbabilities = (coefs, i) => [...new Array(3)].map((item, j) => ((i === j) ? 0 : -coefs[i][j] / coefs[i][i]));
+const getDistribution = (probabilities) => {
+    let distribution = new Array(3).fill(0);
+    distribution[0] = probabilities[0]
+    distribution[1] = distribution[0] + probabilities[1];
+    distribution[2] = 1;
+    return distribution;
+}
+const getEvent = (distribution, probability) => distribution.indexOf(distribution.find(d => probability < d));
+
+module.exports = lab15;

eedfb30eb4328d09abc0c9a749242591e16c4007
ectuser <m.ectuser@gmail.com>
2020-06-24 18:46:48 +0700

    Changed db from file storage to memory storage

--- server/services/lab17.js
+++ server/services/lab17.js
@@ -5,19 +5,21 @@ const main = () => {
     const lambda1 = 0.3;
     const lambda2 = 0.5;
 
-    for (let i = 0; i < 10; i++){
-        t1.push(emulateFlow(lambda1, t1[t1.length - 1]));
-        t2.push(emulateFlow(lambda2, t2[t2.length - 1]));
-    }
-    console.log(t1, t2);
-    const combined = mergeAndSortArrays([...t1], [...t2]);
-    console.log(combined);
+    // for (let i = 0; i < 10; i++){
+    //     t1.push(emulateFlow(lambda1, t1[t1.length - 1]));
+    //     t2.push(emulateFlow(lambda2, t2[t2.length - 1]));
+    // }
+    // console.log(t1, t2);
+    // const combined = mergeAndSortArrays([...t1], [...t2]);
+    // console.log(combined);
 }
 
-const emulateFlow = (lambda, prevT) => {
-    const alpha = 0.5;
-    return prevT + ( -Math.log(alpha) / lambda );
-}
+
+
+// const emulateFlow = (lambda, prevT) => {
+//     const alpha = 0.5;
+//     return prevT + ( -Math.log(alpha) / lambda );
+// }
 
 const mergeAndSortArrays = (first, second) => ([...first, ...second].sort((a, b) => a - b));
 
